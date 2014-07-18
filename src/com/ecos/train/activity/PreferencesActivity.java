9f37358aa4caaad4047aef756d5f4a6027e20328
rk4an <erkan2005@gmail.com>
2014-07-18 23:29:05 +0200

    show value in settings description

--- src/com/ecos/train/activity/PreferencesActivity.java
+++ src/com/ecos/train/activity/PreferencesActivity.java
@@ -20,30 +20,62 @@
 package com.ecos.train.activity;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 import com.ecos.train.R;
 
 
-public class PreferencesActivity extends PreferenceActivity {
+public class PreferencesActivity extends PreferenceActivity implements OnPreferenceChangeListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 
-		//getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferencesFragment()).commit();
 		addPreferencesFromResource(R.xml.prefs);
+		bindPreferenceSummaryToValue(findPreference("ip"));
+		bindPreferenceSummaryToValue(findPreference("port"));
+		bindPreferenceSummaryToValue(findPreference("pref_speed"));
 	}
 
-	/*public static class PreferencesFragment extends PreferenceFragment
-	{
-		@Override
-		public void onCreate(final Bundle savedInstanceState)
-		{
-			super.onCreate(savedInstanceState);
-			addPreferencesFromResource(R.xml.prefs);
+	/**
+	 * Attaches a listener so the summary is always updated with the preference value.
+	 * Also fires the listener once, to initialize the summary (so it shows up before the value
+	 * is changed.)
+	 */
+	private void bindPreferenceSummaryToValue(Preference preference) {
+		// Set the listener to watch for value changes.
+		preference.setOnPreferenceChangeListener(this);
+
+		// Trigger the listener immediately with the preference's
+		// current value.
+		onPreferenceChange(preference,
+				PreferenceManager
+				.getDefaultSharedPreferences(preference.getContext())
+				.getString(preference.getKey(), ""));
+	}
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object value) {
+		String stringValue = value.toString();
+
+		if (preference instanceof ListPreference) {
+			// For list preferences, look up the correct display value in
+			// the preference's 'entries' list (since they have separate labels/values).
+			ListPreference listPreference = (ListPreference) preference;
+			int prefIndex = listPreference.findIndexOfValue(stringValue);
+			if (prefIndex >= 0) {
+				preference.setSummary(listPreference.getEntries()[prefIndex]);
+			}
+		} else {
+			// For other preferences, set the summary to the value's simple string representation.
+			preference.setSummary(stringValue);
 		}
-	}*/
+		return true;
+	}
 
 }
 
